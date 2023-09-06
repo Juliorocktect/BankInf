69c6798e0861f5cc71371438247c507a474f35e6
JK <113830349+Juliorocktect@users.noreply.github.com>
2023-09-06 09:00:10 +0200

    Add files via upload

---
+++ neu/Kunde.java
@@ -0,0 +1,18 @@
+class Kunde{
+private String vorname,name,addresse;
+private int kundenNummer, gebDatum;
+
+public Kunde(String vorname, String addresse,int kundenNummer,int gebDatum)
+{
+    this.vorname = vorname;
+    this.name = name;
+    this.addresse = addresse;
+    this.gebDatum = gebDatum;
+    this.kundenNummer = kundenNummer;
+}
+public void einzahlen(int betrag,int kontoNr){}
+public void auszahlen(int betrag,int kontoNr){}
+public void abheben(int betrag,int pin){}
+public void überweisen(){}
+}
+
